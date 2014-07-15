7b74e39561c7f6e4144e22c59bf4ee483d396089
Frank Beaton <frankbeaton@gmail.com>
2014-07-15 01:08:24 -0700

    converted plugin list to bash script
    - ssia

---
+++ vim/bundle/get_plugins.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/vim-scripts/Align.git $HOME/.vim/bundle/Align;
+git clone https://github.com/jlanzarotta/bufexplorer.git $HOME/.vim/bundle/bufexplorer;
+git clone https://github.com/kien/ctrlp.vim.git $HOME/.vim/bundle/ctrlp.vim;
+git clone https://github.com/scrooloose/nerdtree.git $HOME/.vim/bundle/nerdtree;
+git clone https://github.com/tobyS/pdv.git $HOME/.vim/bundle/pdv;
+git clone https://github.com/mtth/scratch.vim.git $HOME/.vim/bundle/scratch.vim;
+git clone https://github.com/vim-scripts/tComment.git $HOME/.vim/bundle/tComment;
+git clone https://github.com/majutsushi/tagbar.git $HOME/.vim/bundle/tagbar;
+git clone https://github.com/tpope/vim-fugitive.git $HOME/.vim/bundle/vim-fugitive;
+git clone https://github.com/tobyS/vmustache.git $HOME/.vim/bundle/vmustache;
+
+exit 0;
